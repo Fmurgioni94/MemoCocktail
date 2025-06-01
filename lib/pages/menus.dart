286ba0bc4af33ba6505422d14f02e135dd4edc10
Fmurgioni94 <fr.murgioni94@gmail.com>
2025-06-01 17:10:42 +0100

    fix(small issues): nothing relevant for the project i tried to use implement a reading json file.

--- lib/pages/menus.dart
+++ lib/pages/menus.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
-class Menus extends StatelessWidget {
+class Menus extends StatefulWidget {
   const Menus({super.key});
+
+  @override
+  State<Menus> createState() => _MenusState();
+}
+
+
+class _MenusState extends State<Menus> {
+
+  Future<void> loadJsonData() async {
+  final String jsonString = await rootBundle.loadString('assets/data.json');
+  final data = jsonDecode(jsonString);
+  print(data); // You can now use it like a Map or List depending on the JSON
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -28,3 +44,7 @@ class Menus extends StatelessWidget {
     );
   }
 }
+
+  
+
+  
